5d2e33a9d3d37f579f08821dfad0134f250b02fc
eft0 <eft0@gmx.net>
2015-05-26 15:35:24 -0400

    cleaning code

--- class.mysql.php
+++ class.mysql.php
@@ -4,50 +4,50 @@
  * @author eft0 <esteban@fernandez.cl>
  * @version 2.0
  */
-class MySQL 
-{	
+class MySQL
+{
 	private static $conn;
-	
+
 	function __construct($host, $user, $pass, $db = false)
-	{	
+	{
 		self::$conn = mysqli_connect($host, $user, $pass, $db);
 		if (mysqli_connect_errno())
 			  echo "Failed to connect to MySQL: " . mysqli_connect_error();
 		else
 			return self::$conn
-	}	
-	
+	}
+
 	public static function query($query)
 	{
 		$result = mysqli_query(self::$conn, $query);
 		if (!$result) {
 			echo "<pre>" . $query . "</pre>";
 			die (mysqli_error(self::$conn) ." ". mysqli_errno(self::$conn));
-		} else {		
+		} else {
 			return $result;
 		}
 	}
-	
+
 	public static function affected_rows()
 	{
 		return mysqli_affected_rows(self::$conn);
 	}
-	
+
 	public static function fetch_object($result)
 	{
 		$data = mysqli_fetch_object($result);
 		return $data;
 	}
-	
+
 	public static function last_id($result) {
 		return mysqli_insert_id(self::$conn);
 	}
-	
+
 	function __destruct()
 	{
 		if (self::$conn) {
 			mysqli_close(self::$conn);
 		}
-	}	
+	}
 }
 ?>
